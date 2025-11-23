f89edd81aef8c036233a21fb15cfe85f2f20f540
DPIRCI <pircidoga255@gmail.com>
2025-11-23 11:50:37 +0300

    SOLVED #1Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.

---
+++ Medium/3sum.py
@@ -0,0 +1,49 @@
+#Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that
+# i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+from typing import List
+from typing import List
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()   # Sort array
+        n = len(nums)
+        res = []
+
+        for i in range(n - 2):
+            # Skip duplicate
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left, right = i + 1, n - 1
+            target = -nums[i]
+
+            while left < right:
+                s = nums[left] + nums[right]
+
+                if s == target:
+                    # Found a valid triplet
+                    res.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+
+                    # Skip duplicates for left
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    # Skip duplicates for right
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+                elif s < target:
+                    left += 1
+                else:
+                    right -= 1
+
+        return res
+if __name__ == "__main__":
+    nums = [-1,0,1,2,-1,-4]
+    answer = Solution().threeSum(nums)
+    print(f"input: nums=[{nums}]")
+    print(f"output: {answer}")
+
+
+
